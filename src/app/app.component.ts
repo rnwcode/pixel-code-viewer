b0948498a788166099f4a74dcd40e790e38295b3
René Niclas Winkelmann <rene@qikkip.com>
2021-02-10 16:10:24 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pixel-code-viewer';
+
+  style = 'style-1';
+  complexity = 'low';
+  size = 'small';
+  pixelSize = "10";
+
+  pixelData;
+  colorData;
+  widthCount;
+  widthClass;
+
+  constructor(private http: HttpClient) {
+    this.http.get(`https://assignment.triu-software.de/api/${this.style}-${this.complexity}-${this.size}.json`).subscribe((res: any) => {
+      this.widthCount = res.width;
+      this.widthClass = 'grid_' + res.width;
+      this.pixelData = res.data;
+      this.colorData = res.colorTable;
+    });
+  }
+
+  _updateImage() {
+    this.http.get(`https://assignment.triu-software.de/api/${this.style}-${this.complexity}-${this.size}.json`).subscribe((res: any) => {
+      this.widthCount = res.width;
+      this.widthClass = 'grid_' + res.width;
+      this.pixelData = res.data;
+      this.colorData = res.colorTable;
+    });
+  }
+
+  generateColor(index: number) {
+    return `rgba(${this.colorData[index][0]}, ${this.colorData[index][0]}, ${this.colorData[index][0]})`;
+  }
+
+  getPixelSizeClass() {
+    return `${Number(this.pixelSize) * this.widthCount}px`;
+  }
+
+  generateGridColumns() {
+    return `repeat(${this.widthCount}, 1fr)`;
+  }
+
+
 }
